5e2897939cc526f6994f9a6cf0d8d614e5041678
Andreea Dumitru <andreean.dumitru@gmail.com>
2017-04-17 21:53:04 +0300

    Add new dependencies to run migrations. Run in cmd - dotnet ef migrations add InitialDatabase and - dotnet ef database update

---
+++ src/Travel/Migrations/20170417184507_InitialDatabase.Designer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Travel.Models;
+
+namespace Travel.Migrations
+{
+    [DbContext(typeof(TravelContext))]
+    [Migration("20170417184507_InitialDatabase")]
+    partial class InitialDatabase
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Travel.Models.Stop", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("Arrival");
+
+                    b.Property<double>("Latitude");
+
+                    b.Property<double>("Longitude");
+
+                    b.Property<string>("Name");
+
+                    b.Property<int>("Order");
+
+                    b.Property<int?>("TripId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("TripId");
+
+                    b.ToTable("Stops");
+                });
+
+            modelBuilder.Entity("Travel.Models.Trip", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("CreationDate");
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("UserName");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Trips");
+                });
+
+            modelBuilder.Entity("Travel.Models.Stop", b =>
+                {
+                    b.HasOne("Travel.Models.Trip")
+                        .WithMany("Stops")
+                        .HasForeignKey("TripId");
+                });
+        }
+    }
+}
